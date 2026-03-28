b636729ef479e4837dedaa345089574dd671b8f8
glab254-EE <glebvarlam@gmail.com>
2026-03-28 20:25:51 +0500

    Added additional check to Collection to prevent double-invoking.

--- Assets/Core/Statistics/WillPower/WillOrbCollectionBehaviour.cs
+++ Assets/Core/Statistics/WillPower/WillOrbCollectionBehaviour.cs
@@ -7,13 +7,15 @@ public class WillOrbCollectionBehaviour : MonoBehaviour
     [field:SerializeField]
     public float Points {get;private set;}
     public WillpowerManager manager;
+    private bool collected = false;
     void OnTriggerEnter(Collider other)
     {
-        if (other == null || other.gameObject == null) return;
+        if (other == null || other.gameObject == null || collected) return;
         if (LayerMaskUtils.CompareCollisionlayers(other.gameObject.layer,playerlayer))
         {
             if (manager.TryAddPoints(this) && gameObject != null)
             {
+                collected = true;
                 Destroy(gameObject);
             }
         }
